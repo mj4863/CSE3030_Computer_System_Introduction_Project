00782735a9f1be6cd350473b5bee317d6f409004
Minjun Park <78081023+mj4863@users.noreply.github.com>
2025-05-23 01:37:45 +0900

    Add files via upload

---
+++ Lab1/1-2/absVal.c
@@ -0,0 +1,8 @@
+/*
+ * absVal(x): return the absolute value of x.
+ * - Assume that INT_MIN (-2^31) is not provided as an input.
+ * - Ex) absVal(2) = 2, absVal(-1) = 1
+ */
+int absVal(int x) {
+  return 0; // TODO
+}
---
+++ conditional.c
@@ -0,0 +1,10 @@
+/*
+ * conditional(x, y, z): Return z if x is 0, return y otherwise.
+ * - Ex) conditional(2, 4, 5) = 4, conditional(0, 1, 2) = 2
+ */
+int conditional(int x, int y, int z) {
+  // TODO
+  int tmp = ~1 + 1;
+  int a = !x + tmp;
+  return (y & a) | (z & ~a);
+}
